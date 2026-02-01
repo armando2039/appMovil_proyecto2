6fd250da1f2b6d7e4863e813cc0d9e36b3dc654d
armando2039 <armanddupre58@gmail.com>
2026-01-31 21:15:56 -0600

    Segundo commit: despliege de boton like, top bar.

--- app/src/main/java/com/ebc/appmovil_proyecto2/MainActivity.kt
+++ app/src/main/java/com/ebc/appmovil_proyecto2/MainActivity.kt
@@ -6,8 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,10 +17,12 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.ebc.appmovil_proyecto2.components.LikeButton
 import com.ebc.appmovil_proyecto2.components.ThemeToggleIcon
 import com.ebc.appmovil_proyecto2.ui.theme.AppMovil_proyecto2Theme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -26,12 +30,28 @@ class MainActivity : ComponentActivity() {
 
             var dark by rememberSaveable { mutableStateOf(false) }
 
-            AppMovil_proyecto2Theme(darkTheme = dark, dynamicColor = true) {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ThemeToggleIcon(
-                        isDark = dark,
-                        onToggle = {dark = !dark },
-                        modifier = Modifier.padding(innerPadding))
+            AppMovil_proyecto2Theme(
+                darkTheme = dark, dynamicColor = true) {
+                Scaffold(modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            title = { Text(text = "¡Hola Mundo!") },
+                            actions = {
+                                ThemeToggleIcon(
+                                    isDark = dark,
+                                    onToggle = {dark = !dark }
+                                )
+                            }
+                        )
+                    }
+                ) { innerPadding ->
+                    /*
+                         */
+
+                    LikeButton(
+                        modifier = Modifier.padding(innerPadding)
+                            .fillMaxSize()
+                    )
                 }
             }
         }
--- app/src/main/java/com/ebc/appmovil_proyecto2/components/LikeComponents.kt
+++ app/src/main/java/com/ebc/appmovil_proyecto2/components/LikeComponents.kt
@@ -1,14 +1,30 @@
 package com.ebc.appmovil_proyecto2.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material.icons.outlined.ThumbUp
 import androidx.compose.material.icons.rounded.DarkMode
 
 import androidx.compose.material.icons.rounded.LightMode
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 @Preview(showBackground = true)
@@ -32,3 +48,38 @@ fun ThemeToggleIcon(
     }
 }
 
+
+@Preview(showBackground = true)
+@Composable
+fun LikeButton(
+    liked: Boolean = true,
+    modifier: Modifier = Modifier
+) {
+
+    var liked by remember { mutableStateOf(liked) }
+
+    Row(  //acomoda los componenetes de manera Horizontal
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        IconButton(
+            onClick = {liked = !liked}
+        ) {
+            Icon(
+                imageVector = if (liked) Icons.Filled.ThumbUp else Icons.Outlined.ThumbUp,
+                contentDescription = "¡Me gusta!",
+                tint = if (liked) MaterialTheme.colorScheme.primary
+                else MaterialTheme.colorScheme.onSurfaceVariant,
+                modifier = Modifier.size(48.dp)
+            )
+        }
+
+        Text(
+            text = if (liked) "¡Me gusta!" else "¿No me gusta?",
+            style = MaterialTheme.typography.titleMedium
+        )
+        Spacer(modifier = Modifier.width(12.dp))
+
+    }
+}
